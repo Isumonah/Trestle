0df246a2e082830e1ace0c59c7c42e1527f350fc
Anna Isumonah <maisumonah@gmail.com>
2023-02-26 23:12:58 +0100

    changes

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,7 +1,9 @@
+import Nav from "./Nav";
 
 export default function Events(){
     return(
         <div className="events" id="event">
+            <Nav />
             <h1>Under Construction</h1>
         </div>
     )
