cc1f0f84d29908b46ff231c081b0c29f120c0fd7
Anna Isumonah <maisumonah@gmail.com>
2023-03-29 19:17:29 +0100

    few changes

--- src/components/ApplicationReceivedInvestor.js
+++ src/components/ApplicationReceivedInvestor.js
@@ -5,7 +5,7 @@ import quip from "../images/quip.png"
 export default function ApplicationReceivedInvestor(){
     return(
         <div className="application-received-investor bg-white h-screen xl:pt-16 xl:pl-24">
-            <a href="/" className="xl:text-3xl">TRESTLE</a>
+            <a href="/" className="xl:text-3xl the-trestle-logo">TRESTLE</a>
             <h2 className="xl:text-3xl xl:mt-32 text-center">Application Received!</h2>
             <p className="text-center w-5/12 mx-auto mt-6 xl:text-lg opacity-80">Thank you for your interest in joining our community , your application has been duly received, check your mail within the next 48 hours to get the confirmed date and time for your interview with our screening team.</p>
             <p className="xl:text-lg xl:mt-40 text-center">Top businesses to Invest in:</p>
